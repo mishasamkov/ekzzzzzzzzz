370784b034e31ec484dd488daf5c3417f1db49b5
DikiyLexa <DikiyLexa>
2026-02-06 10:33:33 +0500

    w

--- News.cs
+++ News.cs
@@ -8,16 +8,19 @@ namespace Common
 { 
     public class News
     {
+        public string Src { get; set; }
         public string Img {  get; set; }
         public DateTime Date { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public News(string img, DateTime date,string title,string text )
+        
+        public News(string src,string img, DateTime date,string title,string text )
         { 
             this.Img = img;
             this.Date = date;
             this.Title = title;
             this.Text = text;
+            this.Src = src;
         }
 
         //news-card //news-card_img-small  //news-card_date    //news-card_title  //news-card_text
